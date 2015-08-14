76112443a9eab200c40dcb5bad5c40e20d56cc12
Mitchell Gates <md.fragala@gmail.com>
2015-08-14 14:49:20 -0400

    Populate move-in field; update gitignore.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-   // $('.close').parent().parent().css('display','none');
+
+
 var ax = {};
 ax.activate = function($id, $events, $callback) {
    $('[data-ax='+$id+']').on($events, function(){
@@ -20,122 +21,183 @@ ax.negate = function($id, $events, $callback) {
    });
 }
 
-ax.activate('email-form', 'click', function(){
+ax.activate('contact-form', 'click', function(){
    this.css('display','block');
 });
-ax.negate('email-form', 'click', function(){
+ax.negate('contact-form', 'click', function(){
    this.css('display','none');
 });
 
 
+var today, regex = {}, fields = {}, check = {},
+val = $('.field:not(.noval)'),
+req = $('.field[required]'),
+noval = $('.filed.noval'),
+noreq = $('.filed:not([required])'),
+submit = $('.submit[type="submit"]');
 
-// (function(){
+regex['email'] = /[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9]+(\.[a-zA-Z]+)+$/;
+regex['tel'] = /^\(?([0-9]{3})\)?[\-. ]?([0-9]{3})[\-. ]?([0-9]{4})$/;
 
-   var regex = {}, fields = {}, errors = {};
+fields.email = $('.field[type="email"]');
+fields.phone = $('.field[type="tel"]');
+fields.movein = $('.field[name="movein"]');
 
-   regex.email = /[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9]+(\.[a-zA-Z]+)+$/;
-   regex.tel = /^\(?([0-9]{3})\)?[\-. ]?([0-9]{3})[\-. ]?([0-9]{4})$/;
+check['email'] = function(input) {
+   if (input.search(regex['email']) === 0) return true;
+   else return false;
+};
+check['tel'] = function(input) {
+   if (input.search(regex['tel']) === 0) return true;
+   else return false;
+};
 
-   fields.field = $('.field');
-   fields.email = $('input[type="email"]').not('.noval');
-   fields.phone = $('input[type="tel"]').not('.noval');
-   fields.novalidate = $('.field.noval');
-   fields.required = $('.field[required]');
-   fields.notreq = $('.field').not('[required]');
-   fields.submit = $('input[type="submit"].submit');
+/* populate move-in options */
+(function(){
+   var options, option, today, months, month, i = 0;
+   months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+   options = document.createDocumentFragment();
+   today = new Date();
+
+   for (var i=0; i < 12; i++) {
+      month = today.getMonth() + i;
+      if (month > 11) month -= 12;
+      option = document.createElement('option');
+      option.setAttribute('value', months[month]);
+      option.textContent = months[month];
+      options.appendChild(option);
+   }
+   fields.movein.append(options);
+})();
 
 
-   fields.required.focusout(function () {
-      if ($(this).val() !== "") {
-         if ($(this).hasClass('noval')) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var regex = {}, fields = {}, errors = {};
+
+regex.email = /[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9]+(\.[a-zA-Z]+)+$/;
+regex.tel = /^\(?([0-9]{3})\)?[\-. ]?([0-9]{3})[\-. ]?([0-9]{4})$/;
+
+fields.field = $('.field');
+fields.email = $('input[type="email"]').not('.noval');
+fields.phone = $('input[type="tel"]').not('.noval');
+fields.movein = $('select#move-in');
+fields.novalidate = $('.field.noval');
+fields.required = $('.field[required]');
+fields.notreq = $('.field').not('[required]');
+fields.submit = $('input[type="submit"].submit');
+
+
+
+
+
+
+
+
+
+fields.required.focusout(function () {
+   if ($(this).val() !== "") {
+      if ($(this).hasClass('noval')) {
+         $(this).addClass('valid').removeClass('invalid');
+      } else {
+         var $id = $(this).attr('type');
+
+         if ($(this).val().search(regex[$id]) === 0) {
             $(this).addClass('valid').removeClass('invalid');
          } else {
-            var $id = $(this).attr('type');
-
-            if ($(this).val().search(regex[$id]) === 0) {
-               $(this).addClass('valid').removeClass('invalid');
-            } else {
-               $(this).addClass('invalid').removeClass('valid');
-            }
+            $(this).addClass('invalid').removeClass('valid');
          }
-      } else {
-         $(this).addClass('invalid').removeClass('valid');
       }
-   });
-
-   fields.notreq.focusout(function () {
-      if ($(this).val().length === 0) {
-         $(this).removeClass('invalid');
-      }
-   });
-
-   fields.email.focusout(function () {
-		if ($(this).val().search(regex.email) === 0) {
-			$(this).addClass('valid').removeClass('invalid');
-		} else {
-			$(this).addClass('invalid').removeClass('valid');
-		}
-	});
-
-   function enableSubmit() {
-      var $reqs = $('[required]'),
-         $vals = $('.field:not(.noval)'),
-         $reqsValid = $('[required].valid'),
-         $valsNotInvalid = $('.field:not(.noval):not(.invalid)'),
-         clearance = 0;
-
-      if ($reqs.length !== $reqsValid.length) { clearance += 1; }
-      if ($vals.length !== $valsNotInvalid.length) { clearance += 1; }
-
-      if (clearance === 0) {
-         $('.submit').removeAttr('disabled');
-         console.log("clearance === 0");
-      } else {
-         $('.submit').attr('disabled', '');
-         console.log("clearance !== 0");
-      }
+   } else {
+      $(this).addClass('invalid').removeClass('valid');
    }
+});
 
-   $('.submit').mouseenter(function () {
-      var $reqs = $('[required]'),
-         $vals = $('.field:not(.noval)'),
-         $reqsValid = $('[required].valid'),
-         $valsNotInvalid = $('.field:not(.noval):not(.invalid)'),
-         clearance = 0;
+fields.notreq.focusout(function () {
+   if ($(this).val().length === 0) {
+      $(this).removeClass('invalid');
+   }
+});
 
-      if ($reqs.length !== $reqsValid.length) { clearance += 1; }
-      if ($vals.length !== $valsNotInvalid.length) { clearance += 1; }
+fields.email.focusout(function () {
+	if ($(this).val().search(regex.email) === 0) {
+		$(this).addClass('valid').removeClass('invalid');
+	} else {
+		$(this).addClass('invalid').removeClass('valid');
+	}
+});
 
-      if (clearance === 0) {
-         $(this).removeAttr('disabled');
-      } else {
-         $(this).attr('disabled', '');
+function enableSubmit() {
+   var $reqs = $('[required]'),
+      $vals = $('.field:not(.noval)'),
+      $reqsValid = $('[required].valid'),
+      $valsNotInvalid = $('.field:not(.noval):not(.invalid)'),
+      clearance = 0;
+
+   if ($reqs.length !== $reqsValid.length) { clearance += 1; }
+   if ($vals.length !== $valsNotInvalid.length) { clearance += 1; }
+
+   if (clearance === 0) {
+      $('.submit').removeAttr('disabled');
+   } else {
+      $('.submit').attr('disabled', '');
+   }
+}
+
+$('.submit').mouseenter(function () {
+   var $reqs = $('[required]'),
+      $vals = $('.field:not(.noval)'),
+      $reqsValid = $('[required].valid'),
+      $valsNotInvalid = $('.field:not(.noval):not(.invalid)'),
+      clearance = 0;
+
+   if ($reqs.length !== $reqsValid.length) { clearance += 1; }
+   if ($vals.length !== $valsNotInvalid.length) { clearance += 1; }
+
+   if (clearance === 0) {
+      $(this).removeAttr('disabled');
+   } else {
+      $(this).attr('disabled', '');
+   }
+});
+fields.field.mouseleave(function(){
+   enableSubmit();
+});
+
+$('#contact-form').on('submit', function (e) {
+   e.preventDefault();
+   $.ajax({
+      type: 'POST',
+      url: 'submit.php',
+      data: $(this).serialize(),
+      beforeSend: function () {
+         $('.submit').attr('disabled', '').val('SENDING...');
+      },
+      success: function () {
+         $('.submit').val('THANK YOU');
+      },
+      error: function () {
+         $('.submit').removeAttr('disabled');
       }
    });
-   fields.field.mouseleave(function(){
-      enableSubmit();
-   });
-
-   $('#email-form').on('submit', function (e) {
-      e.preventDefault();
-      $.ajax({
-         type: 'POST',
-         url: 'submit.php',
-         data: $(this).serialize(),
-         beforeSend: function () {
-            $('.submit').attr('disabled', '').val('SENDING...');
-         },
-         success: function () {
-            $('.submit').val('THANK YOU');
-         },
-         error: function () {
-            $('.submit').removeAttr('disabled');
-         }
-      });
-   });
-
-// })();
-
+});
 
 
 
